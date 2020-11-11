f89b04d1a6cfbdb3b166c458dafa8a3c1d172e87
André Zaschka <andre.zaschka@vrdirect.com>
2020-11-11 22:37:49 +0100

    Added FileInfo class to ios

--- src/download-progress.ios.ts
+++ src/download-progress.ios.ts
@@ -3,6 +3,11 @@ import { getFilenameFromUrl } from '@nativescript/core/http/http-request/http-re
 
 type ProgressCallback = (progress: number, url: string, destination: string) => void;
 
+type HeaderInformation = {
+    contentLength: number;
+    contentMD5: string;
+};
+
 const currentDevice = UIDevice.currentDevice;
 const device = currentDevice.userInterfaceIdiom === UIUserInterfaceIdiom.Phone ? 'Phone' : 'Pad';
 const osVersion = currentDevice.systemVersion;
@@ -14,6 +19,66 @@ const USER_AGENT = `Mozilla/5.0 (i${device}; CPU OS ${osVersion.replace(
 )} like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/${osVersion} Mobile/10A5355d Safari/8536.25`;
 const sessionConfig = NSURLSessionConfiguration.defaultSessionConfiguration;
 const queue = NSOperationQueue.mainQueue;
+
+export class FileInfo {
+    public getHeaderInformation(url: string): Promise<HeaderInformation> {
+        return new Promise<HeaderInformation>((resolve, reject) => {
+            try {
+                const urlRequest = NSMutableURLRequest.requestWithURL(NSURL.URLWithString(url));
+                urlRequest.setValueForHTTPHeaderField(USER_AGENT, USER_AGENT_HEADER);
+                urlRequest.HTTPMethod = 'HEAD';
+
+                @NativeClass()
+                class DownloadProgressDelegate extends NSObject implements NSURLSessionDataDelegate {
+                    static ObjCProtocols = [NSURLSessionDataDelegate];
+                    private urlResponse: NSURLResponse;
+
+                    public URLSessionDataTaskDidReceiveResponseCompletionHandler(
+                        session: NSURLSession,
+                        dataTask: NSURLSessionDataTask,
+                        response: NSURLResponse,
+                        completionHandler: (p1: NSURLSessionResponseDisposition) => void
+                    ) {
+                        completionHandler(NSURLSessionResponseDisposition.Allow);
+                        this.urlResponse = response;
+                    }
+
+                    public URLSessionTaskDidCompleteWithError(
+                        session: NSURLSession,
+                        task: NSURLSessionTask,
+                        error: NSError
+                    ) {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            const httpResponse = this.urlResponse as NSHTTPURLResponse;
+                            const contentLength: number = httpResponse.allHeaderFields.objectForKey('Content-Length');
+                            const contentMD5: string = httpResponse.allHeaderFields.objectForKey('Content-MD5');
+
+                            const headerInformation: HeaderInformation = {
+                                contentLength: contentLength,
+                                contentMD5: contentMD5,
+                            };
+
+                            resolve(headerInformation);
+                        }
+                    }
+                }
+
+                const session = NSURLSession.sessionWithConfigurationDelegateDelegateQueue(
+                    sessionConfig,
+                    <DownloadProgressDelegate>DownloadProgressDelegate.new(),
+                    queue
+                );
+                const dataTask = session.dataTaskWithRequest(urlRequest);
+
+                dataTask.resume();
+            } catch (ex) {
+                reject(ex);
+            }
+        });
+    }
+}
 
 export class DownloadProgress {
     private progressCallback: ProgressCallback;
